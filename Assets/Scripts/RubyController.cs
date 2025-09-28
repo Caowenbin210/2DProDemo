e48f851bb3991d9c86c47903b334e133fa97f501
Xuyang <210675154@qq.com>
2025-09-28 16:45:24 +0800

    3.角色控制器和键盘输入

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -6,8 +6,12 @@ public class RubyController : MonoBehaviour
 {
     private void Update()
     {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vericial = Input.GetAxis("Vertical");
+
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
+        position.x = position.x + 3f * horizontal * Time.deltaTime;
+        position.y = position.y + 3f * vericial * Time.deltaTime;
         transform.position = position;
     }
 }
